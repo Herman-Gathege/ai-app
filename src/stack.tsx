c36c8784557864d2567ec19767ae327f47245328
Herman-Gathege <172432295+Herman-Gathege@users.noreply.github.com>
2025-06-28 14:38:16 +0300

    fixing auth

--- src/stack.tsx
+++ src/stack.tsx
@@ -13,6 +13,6 @@ export const stackServerApp = new StackServerApp({
   appId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID!,
   clientId: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY!,
   clientSecret: process.env.STACK_SECRET_SERVER_KEY!,
-  baseUrl: "https://api.stackframe.dev",
+  baseUrl: "http://72.5.42.211",
   tokenStore: "nextjs-cookie",
 });
